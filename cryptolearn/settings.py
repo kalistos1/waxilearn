d551868f5230f36c5623cb1b2f6b84ebe0e33d87
kalistos1 <ucktech1@gmail.com>
2022-03-01 18:44:27 +0100

    added postgres database connections

--- cryptolearn/settings.py
+++ cryptolearn/settings.py
@@ -80,13 +80,25 @@ WSGI_APPLICATION = 'cryptolearn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'waxionschool',
+        'USER': 'admin',
+        'PASSWORD': 'kal15t05',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
